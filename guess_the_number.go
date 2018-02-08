9c5de9b6172af96edf2fcffb23f86c62d8055831
Anthony <anthonyhunter2@hotmail.com>
2018-02-07 20:53:22 -0500

    longest 30 lines of code ever

--- guess_the_number.go
+++ guess_the_number.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
+	"time"
 )
 
-//rand.Seed(time.Now().Nanosecond())
-//var number rand.Intn(1,100)
-
-func mainb() {
+func main() {
+	rand.Seed(int64(time.Now().Nanosecond()))
 	number := rand.Intn(100)
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("I'm thinking of a number between 1 and 100, can you guess my number?\n")
-	guess, _ := reader.Reader
-	if guess == number {
-		fmt.Println("OMG YOU guessed my number")
-	} else {
-		too_high(guess)
-		too_low(guess)
+	var input int
+	fmt.Println("I'm thinking of a number between 1 and 100, can you guess my number?")
+	for input != number {
+		_, err := fmt.Scanf("%d", &input)
+		if err != nil {
+			fmt.Println(err)
+		} else if input == number {
+			fmt.Println("OMG YOU guessed my number")
+		} else if tooHigh(input, number) {
+			fmt.Println("Thats too high to be my number, try again")
+		} else if tooLow(input, number) {
+			fmt.Println("Thats too low to my number, try again")
+		}
 	}
 }
 
-func too_high(num int) int {
-	if num > number {
-		fmt.Println("Sorry thats to high to be my number")
-	}
+func tooHigh(num int, rando int) bool {
+	return num > rando
 }
-func too_low(num int) int {
-	if num < number {
-		fmt.Println("Sorry but thats too low to be my number")
-	}
+func tooLow(num int, rando int) bool {
+	return num < rando
 }
