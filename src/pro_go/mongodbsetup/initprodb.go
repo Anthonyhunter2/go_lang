03045e5e41e0b024fc0299cce68eeb3954c58916
steve-horan <stephen.horanjr@pjm.com>
2018-03-01 14:53:40 -0500

    Cleaned up some allocations and removed session copy.

--- src/pro_go/mongodbsetup/initprodb.go
+++ src/pro_go/mongodbsetup/initprodb.go
@@ -11,21 +11,19 @@ var mondb *mgo.Database
 var moncol *mgo.Collection
 
 func initdb() {
+	var err error
 	dbhost := "172.17.0.2"
 	dbname := "proGodb"
 	col := "app"
 	//dbhost := os.Getenv("DBHOST")
 	//dbname := os.Getenv("DBNAME")
 	//col := os.Getenv("COLNAME")
-	constring := dbhost + "/" + dbname
-	connect, err := mgo.Dial(constring)
-	session := connect.Copy()
+	moncon, err = mgo.Dial(dbhost + "/" + dbname)
 	if err != nil {
 		fmt.Println("Couldnt create connection")
 		return
 	}
-	moncon = session
-	mondb = session.DB(dbname)
+	mondb = moncon.DB(dbname)
 	moncol = mondb.C(col)
 
 	//fmt.Print(s.Find(nil).All(&Something))
